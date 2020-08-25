44f8f7a36efb540424f3a80c7e18a7a085646653
LuqmaanKhan <luq.j.khan@gmail.com>
2020-08-25 14:22:08 -0500

    add login vc and create vc

---
+++ Ode/Ode/ViewControllers/CreateOdeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateOdeViewController.swift
+//  Ode
+//
+//  Created by Luqmaan Khan on 8/25/20.
+//  Copyright © 2020 Luqmaan Khan. All rights reserved.
+//
+
+import UIKit
+
+class CreateOdeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .black
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
